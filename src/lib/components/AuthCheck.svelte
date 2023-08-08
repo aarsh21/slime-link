67a3823f2ed782de39c562ae74828e38979c555f
aarsh21 <aarshpersonal21@gmail.com>
2023-08-08 21:05:55 +0530

    add: custom usernames on firebase

---
+++ src/lib/components/AuthCheck.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { user } from '$lib/firebase';
+</script>
+
+{#if $user}
+	<slot />
+{:else}
+	<p class="text-error my-10">
+		you must be logged in to view this page.
+		<br />
+	</p>
+	<a class="btn btn-primary" href="/login"> Sign In </a>
+{/if}
--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,4 +1,105 @@
 <script lang="ts">
+	import { userData } from '$lib/firebase';
+	import AuthCheck from '$lib/components/AuthCheck.svelte';
+	import { db, user } from '$lib/firebase';
+	import { doc, getDoc, writeBatch } from 'firebase/firestore';
+
+	const re = /^(?=[a-zA-Z0-9._]{3,16}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+	$: isValid = username?.length > 2 && username.length < 16 && re.test(username);
+	$: isTouched = username.length > 0;
+	$: isTaken = isValid && !isAvailable && !loading;
+
+	let username = '';
+	let loading = false;
+	let isAvailable = false;
+
+	let debounceTimer: NodeJS.Timeout;
+
+	async function confirmUsername() {
+		console.log('confirming username', username);
+
+		const batch = writeBatch(db);
+		batch.set(doc(db, 'usernames', username), { uid: $user?.uid });
+		batch.set(doc(db, 'users', $user!.uid), {
+			username,
+			photoURL: $user?.photoURL ?? null,
+			published: true,
+			bio: 'I am the slime',
+			links: [
+				{
+					title: 'Test Link',
+					url: 'https://mail.google.com',
+					icon: 'custom'
+				}
+			]
+		});
+
+		await batch.commit();
+
+		username = '';
+		isAvailable = false;
+	}
+	async function checkAvailibilty() {
+		isAvailable = false;
+		clearTimeout(debounceTimer);
+
+		loading = true;
+
+		debounceTimer = setTimeout(async () => {
+			console.log('checking availabilty of', username);
+
+			const ref = doc(db, 'usernames', username);
+			const exists = await getDoc(ref).then((doc) => doc.exists());
+
+			isAvailable = !exists;
+			loading = false;
+		}, 500);
+	}
 </script>
 
-<h2>username</h2>
+<AuthCheck>
+	<h2 class="text-2xl font-pokemon tracking-[0.4rem] mb-4 cursor-pointer">Username</h2>
+
+	{#if $userData?.username}
+		<p class="text-black bg-success p-2">
+			your username is <span class="text-l text-white">@{$userData.username}</span>
+		</p>
+
+		<p>usernames cannot be changed !</p>
+		<button class="btn btn-neutral">
+			<a href="/login/photo">Upload profile Image</a>
+		</button>
+	{:else}
+		<form class="w-2/5" on:submit|preventDefault={confirmUsername}>
+			<input
+				type="text"
+				placeholder="Username"
+				class="input w-full"
+				bind:value={username}
+				on:input={checkAvailibilty}
+				class:input-error={!isValid && isTouched}
+				class:input-warning={isTaken}
+				class:input-success={isAvailable && isValid && !loading}
+			/>
+			<div class="my-4 min-h-16 w-full">
+				{#if loading}
+					<p class="text-secondary">Checking availability of @{username}...</p>
+				{/if}
+
+				{#if !isValid && isTouched}
+					<p class="text-error text-sm">must be 3-16 characters long, alphanumeric only</p>
+				{/if}
+
+				{#if isValid && !isAvailable && !loading}
+					<p class="text-warning text-sm">
+						@{username} is not available
+					</p>
+				{/if}
+
+				{#if isAvailable}
+					<button class="btn btn-success">Confirm username @{username} </button>
+				{/if}
+			</div>
+		</form>
+	{/if}
+</AuthCheck>
