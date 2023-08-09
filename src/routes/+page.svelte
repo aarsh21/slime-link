a487d82d7c6fd934cf33907fb61fec20eb25a646
aarsh21 <aarshpersonal21@gmail.com>
2023-08-09 18:16:59 +0530

    add : cookie based themes from daisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <script lang="ts">
+	import ThemeSelect from '$lib/components/Theme-select.svelte';
 </script>
+
+<ThemeSelect />
--- src/routes/[username]/+page.svelte
+++ src/routes/[username]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import ThemeSelect from '$lib/components/Theme-select.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -8,15 +9,15 @@
 	<title>@{data.username} Links !</title>
 	<meta name="description" content={data.bio} />
 </svelte:head>
-
+<ThemeSelect />
 <main class="prose text-center mx-auto mt-8">
-	<h1 class="text-7xl text-purple-500 mb-5">
+	<h1 class="text-7xl text-primary-focus mb-5">
 		@{data.username}
 	</h1>
 
 	<img src={data.photoURL ?? '/user.png'} alt="photoURL" width="256" class="mx-auto" />
 
-	<p class="text-xl my-8">{data.bio ?? 'no bio yet...'}</p>
+	<p class="text-xl my-8 text-secondary">{data.bio ?? 'no bio yet...'}</p>
 	<ul class="list-none">
 		{#each data.links as item}
 			{@debug item}
