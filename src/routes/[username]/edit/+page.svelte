8b3905329f8d7a1bbe9261da85d70ba2783ee4b3
aarsh21 <aarshpersonal21@gmail.com>
2023-08-30 19:47:48 +0530

    fix: icon for twitter , added auth check

--- src/routes/[username]/edit/+page.svelte
+++ src/routes/[username]/edit/+page.svelte
@@ -8,7 +8,7 @@
 	import { arrayRemove, arrayUnion, doc, setDoc, updateDoc } from 'firebase/firestore';
 	import { writable } from 'svelte/store';
 
-	const icons = ['Twitter', 'YouTube', 'TikTok', 'LinkedIn', 'GitHub', 'Custom'];
+	const icons = ['X', 'YouTube', 'TikTok', 'LinkedIn', 'GitHub', 'Custom'];
 	const formDefaults = {
 		icon: 'custom',
 		title: '',
@@ -68,98 +68,102 @@
 </script>
 
 <main class="max-w-xl mx-auto">
-	<ThemeSelect />
-	{#if $userData?.username == $page.params.username}
-		<h1 class="mx-2 text-2xl font-bold mt-8 mb-4 text-center">Edit your Profile</h1>
+	<AuthCheck>
+		<ThemeSelect />
 
-		<div class="text-center mb-8">
-			<p>
-				Profile Link:
-				<a href={`/${$userData?.username}`} class="link link-accent">
-					https://kung.foo/{$userData?.username}
-				</a>
-			</p>
-		</div>
+		{#if $userData?.username == $page.params.username}
+			<h1 class="mx-2 text-2xl font-bold mt-8 mb-4 text-center">Edit your Profile</h1>
 
-		<div class="text-center my-4">
-			<a class="btn btn-outline btn-xs" href="/login/photo">Change photo</a>
-			<a class="btn btn-outline btn-xs" href={`/${$userData.username}/bio`}>Edit Bio</a>
-		</div>
-
-		<form class="form-control">
-			<label class="label cursor-pointer flex items-start justify-center">
-				<span class="label-text mr-6">
-					<div
-						class="tooltip group-hover:tooltip-open"
-						data-tip="If public, the world can see your profile"
-					>
-						{$userData?.published ? 'Public profile' : 'Private profile'}
-					</div>
-				</span>
-				<input
-					type="checkbox"
-					class="toggle toggle-success"
-					checked={$userData?.published}
-					on:change={toggleProfileStatus}
-				/>
-			</label>
-		</form>
-
-		<SortableList list={$userData?.links} on:sort={sortList} let:item let:index>
-			<div class="group relative">
-				<UserLink {...item} />
-				<button
-					on:click={() => deleteLink(item)}
-					class="btn btn-xs btn-error invisible group-hover:visible transition-all absolute -right-6 bottom-10"
-					>Delete</button
-				>
+			<div class="text-center mb-8">
+				<p>
+					Profile Link:
+					<a href={`/${$userData?.username}`} class="link link-accent">
+						https://slime-link.vercel.app/{$userData?.username}
+					</a>
+				</p>
 			</div>
-		</SortableList>
-		{#if showForm}
-			<form on:submit|preventDefault={addLink} class="bg-base-200 p-6 w-full mx-auto rounded-xl">
-				<select name="icon" class="select select-sm" bind:value={$formData.icon}>
-					{#each icons as icon}
-						<option value={icon.toLowerCase()}>{icon}</option>
-					{/each}
-				</select>
-				<input
-					name="title"
-					type="text"
-					placeholder="Title"
-					class="input input-sm"
-					bind:value={$formData.title}
-				/>
-				<input
-					name="url"
-					type="text"
-					placeholder="URL"
-					class="input input-sm"
-					bind:value={$formData.url}
-				/>
-				<div class="my-4">
-					{#if !titleIsValid}
-						<p class="text-error text-xs">Must have valid title</p>
-					{/if}
-					{#if !urlIsValid}
-						<p class="text-error text-xs">Must have a valid URL</p>
-					{/if}
-					{#if formIsValid}
-						<p class="text-success text-xs">Looks good!</p>
-					{/if}
-				</div>
 
-				<button disabled={!formIsValid} type="submit" class="btn btn-success block">Add Link</button
-				>
+			<div class="text-center my-4">
+				<a class="btn btn-outline btn-xs" href="/login/photo">Change photo</a>
+				<a class="btn btn-outline btn-xs" href={`/${$userData.username}/bio`}>Edit Bio</a>
+			</div>
 
-				<button type="button" class="btn btn-xs my-4" on:click={cancelLink}>Cancel</button>
+			<form class="form-control">
+				<label class="label cursor-pointer flex items-start justify-center">
+					<span class="label-text mr-6">
+						<div
+							class="tooltip group-hover:tooltip-open"
+							data-tip="If public, the world can see your profile"
+						>
+							{$userData?.published ? 'Public profile' : 'Private profile'}
+						</div>
+					</span>
+					<input
+						type="checkbox"
+						class="toggle toggle-success"
+						checked={$userData?.published}
+						on:change={toggleProfileStatus}
+					/>
+				</label>
 			</form>
-		{:else}
-			<button
-				on:click={() => (showForm = true)}
-				class="btn btn-outline btn-info block mx-auto my-4"
-			>
-				Add a Link
-			</button>
+
+			<SortableList list={$userData?.links} on:sort={sortList} let:item let:index>
+				<div class="group relative">
+					<UserLink {...item} />
+					<button
+						on:click={() => deleteLink(item)}
+						class="btn btn-xs btn-error invisible group-hover:visible transition-all absolute -right-6 bottom-10"
+						>Delete</button
+					>
+				</div>
+			</SortableList>
+			{#if showForm}
+				<form on:submit|preventDefault={addLink} class="bg-base-200 p-6 w-full mx-auto rounded-xl">
+					<select name="icon" class="select select-sm" bind:value={$formData.icon}>
+						{#each icons as icon}
+							<option value={icon.toLowerCase()}>{icon}</option>
+						{/each}
+					</select>
+					<input
+						name="title"
+						type="text"
+						placeholder="Title"
+						class="input input-sm"
+						bind:value={$formData.title}
+					/>
+					<input
+						name="url"
+						type="text"
+						placeholder="URL"
+						class="input input-sm"
+						bind:value={$formData.url}
+					/>
+					<div class="my-4">
+						{#if !titleIsValid}
+							<p class="text-error text-xs">Must have valid title</p>
+						{/if}
+						{#if !urlIsValid}
+							<p class="text-error text-xs">Must have a valid URL</p>
+						{/if}
+						{#if formIsValid}
+							<p class="text-success text-xs">Looks good!</p>
+						{/if}
+					</div>
+
+					<button disabled={!formIsValid} type="submit" class="btn btn-success block"
+						>Add Link</button
+					>
+
+					<button type="button" class="btn btn-xs my-4" on:click={cancelLink}>Cancel</button>
+				</form>
+			{:else}
+				<button
+					on:click={() => (showForm = true)}
+					class="btn btn-outline btn-info block mx-auto my-4"
+				>
+					Add a Link
+				</button>
+			{/if}
 		{/if}
-	{/if}
+	</AuthCheck>
 </main>
